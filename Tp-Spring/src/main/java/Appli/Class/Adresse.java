010819bb278ad673b9ca446e7d504c8c846ae138
anais <anais.bruno26@gmail.com>
2021-11-12 12:21:50 +0100

    classe Adresse

---
+++ Tp-Spring/src/main/java/Appli/Class/Adresse.java
@@ -0,0 +1,78 @@
+package Appli.Class;
+
+import java.util.Objects;
+
+public class Adresse {
+	
+ private String voie;
+ private String complement;
+ private String codePostal;
+ private String ville;
+ 
+ 
+ 
+ 
+public Adresse() {
+	super();
+}
+
+
+public Adresse(String voie, String complement, String codePostal, String ville) {
+	super();
+	this.voie = voie;
+	this.complement = complement;
+	this.codePostal = codePostal;
+	this.ville = ville;
+}
+
+
+public String getVoie() {
+	return voie;
+}
+
+
+public void setVoie(String voie) {
+	this.voie = voie;
+}
+
+
+public String getComplement() {
+	return complement;
+}
+
+
+public void setComplement(String complement) {
+	this.complement = complement;
+}
+
+
+public String getCodePostal() {
+	return codePostal;
+}
+
+
+public void setCodePostal(String codePostal) {
+	this.codePostal = codePostal;
+}
+
+
+public String getVille() {
+	return ville;
+}
+
+
+public void setVille(String ville) {
+	this.ville = ville;
+}
+
+
+@Override
+public String toString() {
+	return "Adresse [voie=" + voie + ", complement=" + complement + ", codePostal=" + codePostal + ", ville=" + ville
+			+ "]";
+}
+
+
+
+	
+}
