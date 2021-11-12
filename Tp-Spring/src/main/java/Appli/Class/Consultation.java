94c2894ffd7f4b663270f02f70c479d2d3589872
Biscay <hugo.bcpst@gmail.com>
2021-11-12 12:30:45 +0100

    Consultation

---
+++ Tp-Spring/src/main/java/Appli/Class/Consultation.java
@@ -0,0 +1,11 @@
+package Appli.Class;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consultation {
+ private Long id;
+ private String instruction;
+ private Patient patient;
+ private List<Creneau> creneaux= new ArrayList<Creneau>();
+}
